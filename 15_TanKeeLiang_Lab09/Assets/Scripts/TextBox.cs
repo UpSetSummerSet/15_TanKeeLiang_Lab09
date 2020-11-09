1237c9584d5415dfc938f3dda8e7d53b23cdbc97
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-11-09 19:32:47 +0800

    Done

---
+++ 15_TanKeeLiang_Lab09/Assets/Scripts/TextBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBox : MonoBehaviour
+{
+    public GameObject position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position.GetComponent<Text>().text = "Position: " + GameObject.FindGameObjectWithTag("Character").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.GetComponent<Text>().text = "Position: " + GameObject.FindGameObjectWithTag("Character").transform.position;
+    }
+}
